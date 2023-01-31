ebb9c653f33a80b3b1704e6b18713d77954559bf
unknown <abdoelsharef1@gmail.com>
2023-02-01 00:35:24 +0200

    create views

--- ERP_App/urls.py
+++ ERP_App/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.demo, name="index"),
+    path('', views.index, name="index"),
+    path('books', views.books, name="books"),
+    # path('update', views.books, name="update"),
+    # path('delete', views.books, name="delete"),
 ]
--- ERP_App/views.py
+++ ERP_App/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import *
+
+# def demo(request):
+#     return render(request, 'books.html')
 
 
-def demo(request):
-    return render(request, 'index.html')
+def index(request):
+    categories = Category.objects.all()
+    items = Item.objects.all()
+    context = {"items": items, 'categories': categories}
+    return render(request, 'index.html', context)
+
+
+def books(request):
+    return render(request, 'books.html')
+
+# def update(request):
+#     return render(request, 'update.html')
+
+# def delete(request):
+#     return render(request, 'delete.html')
